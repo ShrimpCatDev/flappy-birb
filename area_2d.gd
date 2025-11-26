7c9a282bc891358e75f052a6aa245f10cceafb2f
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-11-26 12:48:00 -0600

    PIPES

--- area_2d.gd
+++ area_2d.gd
@@ -1,4 +1,4 @@
-extends Parallax2D
+extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	scroll_offset.x-=30*delta
+	position.x-=60*delta
